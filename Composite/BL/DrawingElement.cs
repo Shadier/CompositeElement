87cbe9512dce5075d4229ed642910be97b49eff6
Raúl Alejandro Moreno Santos <ramoreno@scio.local>
2019-09-05 12:27:53 -0500

    first commit

---
+++ Composite/BL/DrawingElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Composite.BL
+{
+    public abstract class DrawingElement
+    {
+        protected string _name;
+
+        // Constructor
+
+        public DrawingElement(string name)
+        {
+            this._name = name;
+        }
+
+        public abstract void Add(DrawingElement d);
+        public abstract void Remove(DrawingElement d);
+        public abstract void Display(int indent);
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,51 @@
+﻿using Composite.BL;
+using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a tree structure 
+
+            CompositeElement root =
+              new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+            root.Add(new PrimitiveElement("Brown Triangle"));
+
+
+            // Create a branch
+
+            CompositeElement comp =
+              new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+
+            CompositeElement compTwo =
+              new CompositeElement("Red Circle");
+            compTwo.Add(new PrimitiveElement("Red Jr MiniCircle"));
+            compTwo.Add(new PrimitiveElement("Black MiniCircle"));
+            comp.Add(compTwo);
+
+
+            root.Add(comp);
+
+            // Add and remove a PrimitiveElement
+
+            PrimitiveElement pe =
+              new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+
+            // Recursively display nodes
+
+            root.Display(1);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
